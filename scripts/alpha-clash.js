48ca1a9cb0c6814b10ae7b1be3065b07a75bb866
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-14 19:31:56 +0600

    complete the toggle part between home section and play-ground section.

---
+++ scripts/alpha-clash.js
@@ -0,0 +1,6 @@
+function play(){
+    // document.getElementById('home').classList.add('hidden');
+    // document.getElementById('play-ground').classList.remove('hidden');
+    hideElementById('home');
+    showElementById('play-ground');
+}
