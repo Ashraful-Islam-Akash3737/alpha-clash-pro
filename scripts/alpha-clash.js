b9dedc1ed23e3aa18b9897d717562c07fb2957f1
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-15 10:46:11 +0600

    complete the part of score incressing using js

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -17,9 +17,33 @@ function handleKeyBoardKeyUpEvent(event) {
     // checked match or not
     if (playerPressed === expectedAlphabet) {
         console.log('you win');
+        // update score
+        // 1:get the current score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
+        // 2: increase the score by 1
+        const newScore = currentScore + 1;
+        console.log(newScore);
+        // 3:show the updated score 
+        currentScoreElement.innerText = newScore;
 
+
+        // start a new round
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
+    }
+    else{
+        // 1:get the life count
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        // 2:reduce the life count
+        const newLife = currentLife - 1;
+        // 3: update the life count
+        currentLifeElement.innerText = newLife;
+        alert('Please, press the right key...');
     }
 }
 // capture keyboard key press
